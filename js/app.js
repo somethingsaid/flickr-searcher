ce4bb7476d522a0f0c9c749ed8238580e1d6ec52
somethingsaid <kwong.billy@gmail.com>
2016-04-28 11:18:43 -0400

    Some styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-angular.module('myApp', ['ngAnimate']
+angular.module('myApp', ['ngAnimate'])
     .controller('myController', function($http, $scope) {
-
+	    /* Prepare endpoint parameters */
+	    var flickrKey = '05b7506e3fd86ae08a540a59e4e7f40d';
+	    var params = {
+		    method: 'flickr.photos.search',
+		    api_key: flickrKey,
+		    tags: $scope.searchTag,
+		    format: 'json',
+		    nojsoncallback: 1
+		}
     });
