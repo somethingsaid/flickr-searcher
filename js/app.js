cff339d63b678d32982c1cce5ad7a74dcc9819dc
somethingsaid <kwong.billy@gmail.com>
2016-04-28 14:02:51 -0400

    more indentation fixes

--- js/app.js
+++ js/app.js
@@ -10,15 +10,15 @@ angular.module('myApp', ['ngAnimate'])
     		if ($scope.searchForm.$valid) {
     			$scope.query = $scope.searchTag;
     			console.log('Valid form: ' + $scope.query);
-			    
-			    /* Prepare endpoint parameters */
-			    var flickrKey = '05b7506e3fd86ae08a540a59e4e7f40d';
-			    var params = {
-				    method: 'flickr.photos.search',
-				    api_key: flickrKey,
-				    tags: $scope.query,
-				    format: 'json',
-				    nojsoncallback: 1
+
+				/* Prepare endpoint parameters */
+				var flickrKey = '05b7506e3fd86ae08a540a59e4e7f40d';
+				var params = {
+					method: 'flickr.photos.search',
+					api_key: flickrKey,
+					tags: $scope.query,
+					format: 'json',
+					nojsoncallback: 1
 				}
 
 				/* Make call to Flickr */
@@ -26,15 +26,15 @@ angular.module('myApp', ['ngAnimate'])
 					url: 'https://api.flickr.com/services/rest',
 					method: 'GET',
 					params: params
-				})
+					})
 				.then(function(response) {
 					$scope.callComplete = true;
 					$scope.photos = response.data.photos.photo;
 					console.log($scope.photos);
 				}, function(response) {
-				    $scope.callError = true;
+					$scope.callError = true;
 				});
-    		}
+			}
     		else {
     			console.log('Invalid');
     		}
