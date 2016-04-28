6c1f74a951020a875855e37bac9cc1f53cd33a26
somethingsaid <kwong.billy@gmail.com>
2016-04-28 12:12:49 -0400

    Working version

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
 angular.module('myApp', ['ngAnimate'])
-    .controller('myController', function($http, $scope) {
-	    /* Prepare endpoint parameters */
-	    var flickrKey = '05b7506e3fd86ae08a540a59e4e7f40d';
-	    var params = {
-		    method: 'flickr.photos.search',
-		    api_key: flickrKey,
-		    tags: $scope.searchTag,
-		    format: 'json',
-		    nojsoncallback: 1
-		}
+    .controller('myController', function($http, $scope, $sce) {
+    	var init = function() {
+    		$scope.callComplete = false;
+    		$scope.callError = false;
+    	}
+
+    	$scope.submit = function() {
+    		init();
+    		if ($scope.searchForm.$valid) {
+    			$scope.query = $scope.searchTag;
+    			console.log('Valid form: ' + $scope.query);
+			    
+			    /* Prepare endpoint parameters */
+			    var flickrKey = '05b7506e3fd86ae08a540a59e4e7f40d';
+			    var params = {
+				    method: 'flickr.photos.search',
+				    api_key: flickrKey,
+				    tags: $scope.query,
+				    format: 'json',
+				    nojsoncallback: 1
+				}
+
+				/* Make call to Flickr */
+				$http( {
+					url: 'https://api.flickr.com/services/rest',
+					method: 'GET',
+					params: params
+				})
+				.then(function(response) {
+					$scope.callComplete = true;
+					$scope.photos = response.data.photos.photo;
+					console.log($scope.photos);
+				}, function(response) {
+				    $scope.callError = true;
+				});
+    		}
+    		else {
+    			console.log('Invalid');
+    		}
+    	}
     });
